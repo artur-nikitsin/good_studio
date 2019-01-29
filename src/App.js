452cb28cb51cebbe96157ef20b5e31a1fb3ba7f5
Artur Nikitsin <nikitsin.artur@gmail.com>
2019-01-29 21:11:55 +0300

    create-react-app - created starting structure of project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Main from './Main';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <Main/>
+    );
+  }
+}
+
+export default App;
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello</p>
+            </div>
+        );
+    }
+}
+
+export default Main;
